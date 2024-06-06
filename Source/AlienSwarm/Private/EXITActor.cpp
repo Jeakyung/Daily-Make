6519aaa1e236f0a57db1f15756a14adf414e32fb
Choi1205 <159858583+Choi1205@users.noreply.github.com>
2024-06-07 01:12:03 +0900

    [Add] 0606 최환록
    대기 레벨 제작.
    게임 종료/네트워크 끊기 액터 제작 완료
    뷰포트 테스트로는 잘 됨

---
+++ Source/AlienSwarm/Private/EXITActor.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EXITActor.h"
+#include "Components/BoxComponent.h"
+#include "../AlienSwarmCharacter.h"
+#include "Net/UnrealNetwork.h"
+#include "TestPlayerController.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AEXITActor::AEXITActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	rootComp = CreateDefaultSubobject<USceneComponent>(TEXT("RootComp"));
+	SetRootComponent(rootComp);
+
+	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComp"));
+	BoxComp->SetupAttachment(rootComp);
+
+
+}
+
+// Called when the game starts or when spawned
+void AEXITActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	BoxComp->OnComponentBeginOverlap.AddDynamic(this, &AEXITActor::EXIT);
+}
+
+// Called every frame
+void AEXITActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AEXITActor::EXIT(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	AAlienSwarmCharacter* playerREF = Cast<AAlienSwarmCharacter>(OtherActor);
+	if (playerREF) {
+		if (HasAuthority()) {
+			ATestPlayerController* pc = Cast<ATestPlayerController>(playerREF->Controller);
+			pc->ClientReturnToMainMenuWithTextReason(NSLOCTEXT("HostOutMsg", "HostOut", "Host Exited!"));
+			UGameplayStatics::OpenLevel(GetWorld(), TEXT("TitleLevel"));
+		}
+		else {
+			UGameplayStatics::OpenLevel(GetWorld(), TEXT("TitleLevel"));
+		}
+	}
+}
+
